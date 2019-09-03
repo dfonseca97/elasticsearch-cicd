0781436418205f0491b867f3938a8d0257d5aa10
Diego Fonseca <diego.fonseca@endava.com>
2019-09-03 10:11:24 -0500

    update build.gradle and test/integrationTest.sh
    Added the test task to gradle and updated the test script to run the containers and stop them when finished.

--- test/integrationTest.sh
+++ test/integrationTest.sh
@@ -4,8 +4,10 @@ curl -s -d "docsPath=TestSample" http://localhost:8080/index 1>/dev/null
 RESULT=$(curl -s -d "query=academic" http://localhost:8080/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
 if [ "$RESULT" -eq "$EXPECTED" ]; then
     echo true
+    docker-compose down
     exit 0
 else
     echo false
+    docker-compose down
     exit 1
 fi
