75bac9005a954e202318a96a82ea7a97bbf91c3a
Diego Fonseca <diego.fonseca@endava.com>
2019-09-03 11:48:26 -0500

    update docker-compose.yml, update test/integrationTest.sh and add test/dockercompose.yml
    the production docker compose has a volume for the db data. The test now uses a different docker compose file with no volume and updated the integrationTest.sh file to use the new docker compose.

--- test/integrationTest.sh
+++ test/integrationTest.sh
@@ -1,13 +1,14 @@
-EXPECTED=10
-docker-compose up -d
+EXPECTED=1
+docker-compose -f test/docker-compose.yml up -d
+sleep 20
 curl -s -d "docsPath=TestSample" http://localhost:8080/index 1>/dev/null
 RESULT=$(curl -s -d "query=academic" http://localhost:8080/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
 if [ "$RESULT" -eq "$EXPECTED" ]; then
     echo true
-    docker-compose down
+    docker-compose -f test/docker-compose.yml down
     exit 0
 else
     echo false
-    docker-compose down
+    docker-compose -f test/docker-compose.yml down
     exit 1
 fi
