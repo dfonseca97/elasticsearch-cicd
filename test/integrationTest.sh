4ac6d2a003a0d9a492fa38fec3d2c89ac84f3898
Diego Fonseca <diego.fonseca@endava.com>
2019-09-04 10:11:34 -0500

    Update integrationtest.sh
    Added the correct server to make the request

--- test/integrationTest.sh
+++ test/integrationTest.sh
@@ -2,19 +2,19 @@
 #Expected test result.
 EXPECTED=1
 
-export ELASTIC_SERVER=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+export ELASTIC_SERVER=172.19.167.158
 export ELASTIC_SERVER_PORT=9200
-echo $ELASTIC_SERVER
+
 docker-compose -f test/docker-compose.yml up -d
 
 #Wait for the Elastic Search server to start.
 sleep 20
 
 #Insert the test documents into the database.
-curl -s -d "docsPath=TestSample" http://localhost:8080/index 1>/dev/null
+curl -s -d "docsPath=TestSample" http://${ELASTIC_SERVER}:8080/index 1>/dev/null
 
 #Test a simple query on the indexed files.
-RESULT=$(curl -s -d "query=academic" http://localhost:8080/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
+RESULT=$(curl -s -d "query=academic" http://${ELASTIC_SERVER}:8080/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
 
 #Check the obtained result is equal to the expected and drop the environment.
 if [ "$RESULT" -eq "$EXPECTED" ]; then
