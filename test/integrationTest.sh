4484751d0c4a735dcad7c215a41e3c9abd9aa230
Diego Fonseca <diego.fonseca@endava.com>
2019-09-04 10:51:46 -0500

    update test/integrationtest.sh
    added a wait time for the files to upload

--- test/integrationTest.sh
+++ test/integrationTest.sh
@@ -1,4 +1,3 @@
-
 #Expected test result.
 EXPECTED=1
 
@@ -8,13 +7,14 @@ export ELASTIC_SERVER_PORT=9200
 docker-compose -f test/docker-compose.yml up -d
 
 #Wait for the Elastic Search server to start.
-sleep 40
+sleep 35
 
 #Insert the test documents into the database.
-curl -s -d "docsPath=TestSample" http://${ELASTIC_SERVER}:8080/index 1>/dev/null
+curl -d "docsPath=TestSample" http://${ELASTIC_SERVER}:8080/index 1>/dev/null
 
+sleep 10
 #Test a simple query on the indexed files.
-RESULT=$(curl -s -d "query=academic" http://${ELASTIC_SERVER}:8080/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
+RESULT=$(curl -d "query=academic" http://${ELASTIC_SERVER}:8080/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
 
 #Check the obtained result is equal to the expected and drop the environment.
 if [ "$RESULT" -eq "$EXPECTED" ]; then
