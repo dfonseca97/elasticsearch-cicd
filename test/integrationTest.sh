66e4e6efa8b7d1d43cde4d231f0c11cd1e143c74
Diego Fonseca <diego.fonseca@endava.com>
2019-09-04 17:40:07 -0500

    update docker-compose and test folder
    Tests now work with the docker service name

--- test/integrationTest.sh
+++ test/integrationTest.sh
@@ -1,9 +1,10 @@
 #Expected test result.
 EXPECTED=1
 
-export ELASTIC_SERVER=test_elastic
-export ELASTIC_SERVER_PORT=9201
-export SPRING_SERVER_PORT=8989
+export ELASTIC_SERVER=elastic-server-test
+export ELASTIC_SERVER_PORT=9200
+export SPRING_SERVER=spring-app-test
+export SPRING_SERVER_PORT=8080
 
 docker-compose -f test/docker-compose.yml up -d
 
@@ -11,11 +12,11 @@ docker-compose -f test/docker-compose.yml up -d
 sleep 35
 
 #Insert the test documents into the database.
-curl -d "docsPath=TestSample" http://${ELASTIC_SERVER}:${SPRING_SERVER_PORT}/index 1>/dev/null
+curl -d "docsPath=TestSample" http://${SPRING_SERVER}:${SPRING_SERVER_PORT}/index 1>/dev/null
 
 sleep 10
 #Test a simple query on the indexed files.
-RESULT=$(curl -d "query=academic" http://${ELASTIC_SERVER}:${SPRING_SERVER_PORT}/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
+RESULT=$(curl -d "query=academic" http://${SPRING_SERVER}:${SPRING_SERVER_PORT}/search | grep -o p01_theSemanticWeb | grep -c p01_theSemanticWeb)
 
 #Check the obtained result is equal to the expected and drop the environment.
 if [ "$RESULT" -eq "$EXPECTED" ]; then
