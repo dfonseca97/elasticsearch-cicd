9fa546c319d90180248dd78384ebc67dd3dee53d
Diego Fonseca <diego.fonseca@endava.com>
2019-09-05 15:36:35 -0500

    update run_job.sh
    adjusted time for jenkins to start

--- jenkins/run_job.sh
+++ jenkins/run_job.sh
@@ -1,5 +1,5 @@
 echo "---------------------sleep---------------------------"
-sleep 40
+sleep 45
 curl -X POST --user ${JENKINS_USER}:${JENKINS_PASSWORD} http://jenkins:8080/job/${JOB_NAME_1}/build
 echo "------------------job------------------------------"
 java -jar /elasticsearch/build/libs/*.jar
